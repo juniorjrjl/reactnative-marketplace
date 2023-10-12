e5f39c29feb694addb8c7f2c581596ee95426c37
Junior <junior.jr.jl@gmail.com>
2023-10-12 00:23:32 -0300

    feat: ajuste de url nos produtos do usuário

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,10 +1,12 @@
+const fixImageUrl = require("../helpers/fixImagePath")
 const getUserWithProducts = require("../services/get-user-with-products")
 
 module.exports = {
   show: async function (req, res) {
     const { _id } = req.params
     try {
-      const user = await getUserWithProducts(_id)
+      let user = await getUserWithProducts(_id)
+      user.products = fixImageUrl(user.products)
       return res.json(user)
     } catch (err) {
       return res.status(400).json({ error: err.message })
